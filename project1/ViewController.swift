a3a19120aa40adf1f0ba3b66606923549392a0ad
Chris Gonzaga <chrisg.gonzaga@gmail.com>
2018-03-12 11:28:24 -0400

    Initial Commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Chris Gonzaga on 3/12/18.
+//  Copyright © 2018 Chris Gonzaga324243. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
